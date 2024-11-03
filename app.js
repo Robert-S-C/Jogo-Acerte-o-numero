4fb9ea2d3dfad49ab4ab67620c10a80699c590dc
Robert-S-C <charearl2@gmail.com>
2024-11-03 17:56:18 -0300

    Update app.js
    modifiquei um texto que estava errado.

--- app.js
+++ app.js
@@ -61,5 +61,5 @@ function reiniciarJogo() {
 }
 function exibirMensagemInicial() {
     exibirTextoNaTela ('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10.');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100.');
 }
